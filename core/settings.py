84027c4bf09defa3f93633a32f1d16dcbae6948a
Hasan-maqsood13 <hasanmaqsood13@gmail.com>
2025-09-05 15:06:29 +0500

    Initial Commit: Completed the Basis Setup of the project

--- core/settings.py
+++ core/settings.py
@@ -140,5 +140,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'          
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'hasanmaqsood13@gmail.com' 
-EMAIL_HOST_PASSWORD = 'create app in google and add the password here'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD') 
