b094e489c4e3ade97c76d0307af30354f673ba23
Hasan-maqsood13 <hasanmaqsood13@gmail.com>
2025-09-05 15:02:26 +0500

    Initial Commit: Completed the Basis Setup of the project

---
+++ todoapp/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+from django.utils import timezone
+from django.utils.text import slugify
+from django.db.models import CharField, DateField, DecimalField, ImageField
+import uuid
+# Create your models here.
